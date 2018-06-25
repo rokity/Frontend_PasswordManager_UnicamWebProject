cc6c5f47177be17c05a2b262eeb7cca0fbbf2b69
Alepacox <alessandro.pacini@studenti.unicam.it>
2018-06-25 23:32:00 +0200

    Redefined routing page home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from '../components/login/login.component';
+import { HomeComponent } from '../components/home/home.component'
 
 const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
