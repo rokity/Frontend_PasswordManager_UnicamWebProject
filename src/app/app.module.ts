48f5aaf628de87b8b1f81b734f28ce56b06f472d
rokity <riccardo.amadio1@gmail.com>
2018-06-25 23:08:28 +0200

    test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from '../components/login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/components/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+ 
+@Component({
+  selector: 'login-component',
+  templateUrl: './login.component.html',
+})
+export class LoginComponent implements OnInit {
+  constructor() { 
+      alert("heeee")
+  }
+ 
+  ngOnInit() {
+    
+  }
+ 
+}
