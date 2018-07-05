0b792f15c892c1ffc43c42d193a046f47cb149f5
Alepacox <alessandro.pacini@studenti.unicam.it>
2018-07-05 15:30:07 +0200

    added some graphic changes

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'login-component',
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 
 })
 export class LoginComponent  {
