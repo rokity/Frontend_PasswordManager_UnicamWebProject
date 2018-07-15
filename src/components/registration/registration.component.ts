08ef9121a64fd621f8c5f1a252decfc0acb79028
Alepacox <alessandro.pacini@studenti.unicam.it>
2018-07-15 17:31:06 +0200

    Added complexity check for masterkey in registration

--- src/components/registration/registration.component.ts
+++ src/components/registration/registration.component.ts
@@ -38,24 +38,25 @@ export class RegistrationComponent {
       this.router.navigate(['/']);
   }
   registration() {
-    if((this.name && this.surname && this.email && this.masterkey)!= undefined){
-    if (this.emailValidation()) {
-      if (this.checkPswConfirm()) {
-        this.http.post(this.server, { name: this.name, surname: this.surname, email: this.email, masterkey: this.masterkey })
-          .subscribe(data => {
-            if (data['logged'])
-            {
-              localStorage.setItem('token',data['token'])
-              this.router.navigate(['/']);
-            }
-              
-            else if (data['mailUsed']){
-              alert("Email già utilizzata");
-            }
-          })
-      } else alert("Le password inserite non combaciano");
-    } else alert("Inserisci una mail valida");
-  } else alert ("Compila tutti i campi");
+    if ((this.name && this.surname && this.email && this.masterkey) != undefined) {
+      if (this.emailValidation()) {
+        if (this.isStrongPwd()) {
+          if (this.checkPswConfirm()) {
+            this.http.post(this.server, { name: this.name, surname: this.surname, email: this.email, masterkey: this.masterkey })
+              .subscribe(data => {
+                if (data['logged']) {
+                  localStorage.setItem('token', data['token'])
+                  this.router.navigate(['/']);
+                }
+
+                else if (data['mailUsed']) {
+                  alert("Email già utilizzata");
+                }
+              })
+          } else alert("Le password inserite non combaciano");
+        } else alert("La masterkey non rispetta i requisiti di sicurezza")
+      } else alert("Inserisci una mail valida");
+    } else alert("Compila tutti i campi");
   }
 
   checkPswConfirm() {
@@ -67,6 +68,12 @@ export class RegistrationComponent {
   emailValidation() {
     var regMail = /^([_a-zA-Z0-9-]+)(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,3})$/;
     return regMail.test(this.email);
+  }
+
+  isStrongPwd() {
+    var regExp = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[,!@#$%&*().]).{8,}/;
+    var validPassword = regExp.test(this.masterkey);
+    return validPassword;
   }
 
 }
