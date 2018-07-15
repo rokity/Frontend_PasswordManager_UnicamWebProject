66ed3c496c66ea15be76fea30f35e29b98c090e3
Alepacox <alessandro.pacini@studenti.unicam.it>
2018-07-16 00:32:54 +0200

    Starting homepage details definition

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -8,18 +8,12 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   animations: [
-    // the fade-in/fade-out animation.
     trigger('simpleFadeAnimation', [
-      // the "in" style determines the "resting" state of the element when it is visible.
       state('in', style({ opacity: 1 })),
-
-      // fade in when created. this could also be written as transition('void => *')
       transition(':enter', [
         style({ opacity: 0 }),
         animate(600)
       ]),
-
-      // fade out when destroyed. this could also be written as transition('void => *')
       transition(':leave',
         animate(600, style({ opacity: 0 })))
     ])]
