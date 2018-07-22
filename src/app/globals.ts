aa0d34b57972921ade1a68e6152b5373ee2f5fdb
Alepacox <alessandro.pacini@studenti.unicam.it>
2018-07-22 18:41:54 +0200

    Modified server address to localhost by default

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,3 +1,5 @@
 'use strict';
 
-export const server: string="serverAddress:port";  
+export const server: string="localhost:8000";  
+
+//default set to localhost:8000. Change it depending on server address and port
